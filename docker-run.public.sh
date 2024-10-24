8262188c9f7129878ac8154f68ee03b793eaa3b5
ivanzzeth <ivanzz.eth@gmail.com>
2024-10-24 18:56:11 +0800

    Update docker

--- docker-run.public.sh
+++ docker-run.public.sh
@@ -2,4 +2,5 @@
 docker rm -f ethereum-jsonrpc-gateway
 
 docker run -d --name ethereum-jsonrpc-gateway -p 3005:3005 \
+    --restart always \
     ivanzz/ethereum-jsonrpc-gateway
--- docker-run.sh
+++ docker-run.sh
@@ -3,4 +3,5 @@ docker rm -f ethereum-jsonrpc-gateway
 
 docker run -d --name ethereum-jsonrpc-gateway -p 3005:3005 \
     -v ./config.json:/usr/src/app/config.json \
+    --restart always \
     ivanzz/ethereum-jsonrpc-gateway
